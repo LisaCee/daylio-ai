3a8ca083f486ee6221ddef36cba9d6e85fab1de8
LisaCee <lisaceedev@gmail.com>
2025-06-01 15:11:23 -0700

    Add middleware to handle unauthenticated requests
    - Implemented a middleware function to redirect guests with a JSON response indicating 'Unauthenticated.' and a 401 status code.
    - This enhancement improves user experience by providing clear feedback for unauthorized access attempts.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+    $middleware->redirectGuestsTo(function ($request) {
+        return response()->json([
+            'message' => 'Unauthenticated.'
+        ], 401);
+    });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
